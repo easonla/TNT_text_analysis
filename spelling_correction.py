fdbc5c42e36a5951f3f9b90658fe17088d5b9646
easonla <yhh281@nyu.edu>
2018-02-13 16:07:20 -0500

    remove irrelevent files

--- spelling_correction.py
+++ spelling_correction.py
@@ -31,7 +31,7 @@ def correction(word):
 
 def candidates(word): 
     "Generate possible spelling corrections for word."
-    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word] or chkr.suggest(word))
 
 def known(words): 
     "The subset of `words` that appear in the dictionary of WORDS."
